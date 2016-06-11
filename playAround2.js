9da1a8fe4fe36bc094abff10dc5a72c52e51dfae
Howard Hardiman <hvhardiman@gmail.com>
2016-06-11 05:11:21 +0000

    Word check with text box and gameBoard objects array added

--- playAround2.js
+++ playAround2.js
@@ -1,6 +1,7 @@
-const htmlFontsize = 16; //usse this to handle 
+const htmlFontsize = 10; //usse this to handle 
 var wordArray = [];
 var playedWord ="apple";
+var gameBoard = [];
 
 $(document).ready(function() {
 
@@ -8,23 +9,25 @@ $(document).ready(function() {
 // Create Spaces that go on the board, logical plus styling aspects
 for (var n = 0; n <= 11; n++){
     for (var j = 0; j <= 11; j++){
-
+        //Draw Main Play Board here
+        //Since One Time Deal, will leave here instead of 'draw' function
         d = document.createElement('div');
-
         var logicalX = j;
         var realX = j*5.4*htmlFontsize;
         var logicalY = n;
         var realY = n*5.4*htmlFontsize;
-
         $(d).css("left", realX);
         $(d).css("top", realY);
-
         $(d).addClass("boardspace");
         $(d).appendTo($(".container"));
-  //    jQuery('<div class = boardspace/>', {
-  //        top: "0rem", 
-  //        left: "5.4rem"
-        // }).appendTo(".container");   
+
+        //Create the Game board Objects
+        var space = new Object();
+        space.xVal = logicalX;
+        space.yVal = logicalY;
+        space.letterVal = "";
+        space.bonus = "none";
+        gameBoard.push(space);
     }            
 }
 //  
@@ -36,35 +39,34 @@ for (var n = 0; n <= 11; n++){
 
 getFile();
 
-
-
-
-
-
-// $.ajax({
-//             url : "wordsEn.txt",
-//             success : function (data) {
-//                 // $(".container").html(data);
-//                 var lines = data.responseText.split("\n");
-//                 alert(lines.length);
-
-//                 for (var i = 0, len = lines.length; i < len; i++) {
-           
-
-//                 }
-//             }
-//         });       
-
-
+$( ".form" ).click(function() {
+    var found = 0;
+    for(i = 0; i < wordArray.length; i++){
+        testString = wordArray[i].replace(/(\r\n|\n|\r)/gm,"");
+        //alert(testString);
+        if($(".tBox").val() == testString){
+            found = 1;
+            $(".tBox").css("background-color","green");
+        }
+    } 
+    if(!found){
+        $(".tBox").css("background-color","red");
+    }   
 });
+
+for(i=0;i<gameBoard.length; i++){
+    console.log(gameBoard[i].xVal);
+    console.log(gameBoard[i].yVal);
+}
+
+
+
+
+});//end of .ready
 
 
 function getFile(){
     var file = "./wordsEn.txt";
-
-    //ul = document.createElement('ul'); //First creat ul that will hold li's for wordlist
-    //$(ul).addClass("wordList"); //display none in WordList css
-    //$(ul).appendTo("html"); //Add to html dom element
     $.get(file,function(txt){
        
         var lines = txt.split("\n");
@@ -73,9 +75,7 @@ function getFile(){
             wordArray[i]=lines[i];
         }
         // alert(wordArray.length);
-        // wordArray.forEach(function(entry) {
-        // alert(entry);
-        // });
+
 
 
     }); 
