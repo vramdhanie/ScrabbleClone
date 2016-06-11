39cad1a14b66c47c19256e389cfa9059ba447731
Howard Hardiman <hvhardiman@gmail.com>
2016-06-11 05:27:26 +0000

    Refactor

--- playAround2.js
+++ playAround2.js
@@ -3,42 +3,15 @@ var wordArray = [];
 var playedWord ="apple";
 var gameBoard = [];
 
-$(document).ready(function() {
-
-//while(wordArray.length ==0){}
-// Create Spaces that go on the board, logical plus styling aspects
-for (var n = 0; n <= 11; n++){
-    for (var j = 0; j <= 11; j++){
-        //Draw Main Play Board here
-        //Since One Time Deal, will leave here instead of 'draw' function
-        d = document.createElement('div');
-        var logicalX = j;
-        var realX = j*5.4*htmlFontsize;
-        var logicalY = n;
-        var realY = n*5.4*htmlFontsize;
-        $(d).css("left", realX);
-        $(d).css("top", realY);
-        $(d).addClass("boardspace");
-        $(d).appendTo($(".container"));
-
-        //Create the Game board Objects
-        var space = new Object();
-        space.xVal = logicalX;
-        space.yVal = logicalY;
-        space.letterVal = "";
-        space.bonus = "none";
-        gameBoard.push(space);
-    }            
-}
-//  
-// End of create tiles functinoality
-
-
-//Read in from file, raw and create divs
-
-
+//Go ahead and start getting the large file ASAP
 getFile();
 
+$(document).ready(function() {
+    createBoardspaces();
+});//end of .ready
+
+
+//When the "p" is clicked check whatever word is in the box and change the text box color
 $( ".form" ).click(function() {
     var found = 0;
     for(i = 0; i < wordArray.length; i++){
@@ -54,17 +27,41 @@ $( ".form" ).click(function() {
     }   
 });
 
-for(i=0;i<gameBoard.length; i++){
-    console.log(gameBoard[i].xVal);
-    console.log(gameBoard[i].yVal);
+
+function createBoardspaces(){
+
+    // Create Spaces that go on the board, logical plus styling aspects
+    for (var n = 0; n <= 11; n++){
+        for (var j = 0; j <= 11; j++){
+            //Draw Main Play Board here
+            //Since One Time Deal, will leave here instead of 'draw' function
+            d = document.createElement('div');
+            var logicalX = j;
+            var realX = j*5.4*htmlFontsize;
+            var logicalY = n;
+            var realY = n*5.4*htmlFontsize;
+            $(d).css("left", realX);
+            $(d).css("top", realY);
+            $(d).addClass("boardspace");
+            $(d).appendTo($(".container"));
+
+            //Create the Game board Objects
+            var space = new Object();
+            space.xVal = logicalX;
+            space.yVal = logicalY;
+            space.letterVal = "";
+            space.bonus = "none";
+            gameBoard.push(space);
+        }            
+}
+//  
+// End of create tiles functinoality
+
+
+
 }
 
-
-
-
-});//end of .ready
-
-
+//function that gets the checker file form the server and loads it in memory inside wordArray 
 function getFile(){
     var file = "./wordsEn.txt";
     $.get(file,function(txt){
