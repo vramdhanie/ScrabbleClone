f5f8f3a656c3500abc1233a5b694834f3bf75513
Howard Hardiman <hvhardiman@gmail.com>
2016-06-12 21:31:24 +0000

    Feeling better - event listners added to each tile and board - verified on console

--- playAround2.js
+++ playAround2.js
@@ -8,110 +8,44 @@ var consonants = "bcdfghjklmnpqrstvwxyz";
 var vowels = "aeiou";
 var selectedTileobject;
 
-//Go ahead and start getting the large file ASAP
+//Go ahead and start getting the large file ASAP and setting up objects and DOM
+//need to do this first to make adding even listners less of a headache
 createwordArray();
+createTilelist();
+createBoardspaces();
+
 
 $(document).ready(function() {
-    createBoardspaces();
-    createTilelist();
-    drawGamecomponents();
+    
+    //ready for update and draw here
+
 });//end of .ready
 
-
-
-
-
-
-//Event Listners
-//Event Listners
-//Event Listners
-//Event Listners
-//When the "p" is clicked check whatever word is in the box and change the text box color - Tester
-$( ".form" ).click(function() {
-    var found = 0;
-    for(i = 0; i < wordArray.length; i++){
-        testString = wordArray[i].replace(/(\r\n|\n|\r)/gm,"");
-        //alert(testString);
-        if($(".tBox").val() == testString){
-            found = 1;
-            $(".tBox").css("background-color","green");
-        }
-    } 
-    if(!found){
-        $(".tBox").css("background-color","red");
-    }   
-});
-
-
-$(".container").mouseup(function(event) {
-    		// console.log("mouseup on " +  $(d).css("left") + " " + $(d).css("top"));
-	
-	//console.log("Mouse up"); 
-	console.log("client X: " + event.clientX + " Y:  " + event.clientY); 	
-	var x = event.pageX - $(".container").offset().left;
-  	var y = event.pageY - $(".container").offset().top;	
-  	console.log("relX : " + x + " Y:  " + y);
-
-});
-
-$(".tileArea").click(function(event) {
-    		// console.log("mouseup on " +  $(d).css("left") + " " + $(d).css("top"));
-	
-	//console.log("Tile Area Clicked"); 
-	//console.log("client X: " + event.clientX + " Y:  " + event.clientY); 	
-	var relX = event.pageX - $(".tileArea").offset().left;
-  	var relY = event.pageY - $(".tileArea").offset().top;	
-  	//console.log("relX : " + relX + " Y:  " + relY);
-
-  	for(var i=0; i < 30; i++){
-  		
-		if(isIntersectingsquare(relX, relY, tileList[i].xVal, tileList[i].yVal, 5.4*htmlFontsize)){
-            selectedTileobject = i;
-            alert("Tile Pressed " + i);
-
-		}
-	}
-  	//Determine which tile has been selected
-
-
-  	
-});
-
-
-
-
-
-//End Event Listners
-//End Event Listners
-//End Event Listners
-//End Event Listners
-
-
-
 //Methods
 //Methods
 //Methods
 //Methods
 //Methods
-function drawGamecomponents(){
 
-	for(var i=0; i < 30; i++){
-		if(tileList[i].played ==0){
-			d = document.createElement('div');
-			$(d).css("left", tileList[i].realX);
-            $(d).css("top", tileList[i].realY);
-            $(d).text(tileList[i].value);
-            $(d).addClass("onboardFormat");
-            $(d).addClass("boardspace");
-            $(d).addClass("tileNatural");
-            $(d).appendTo($(".tileArea"));
-		}
-        console.log(tileList[i].value);
-    }
+// function drawGamecomponents(){
 
-}
+// 	for(var i=0; i < 30; i++){
+// 		if(tileList[i].played ==0){//Only display tiles that have not been played here
+// 			d = document.createElement('div');
+// 			$(d).css("left", tileList[i].homeX);
+//             $(d).css("top", tileList[i].homeY);
+//             $(d).text(tileList[i].value);
+//             $(d).addClass("onboardFormat");
+//             $(d).addClass("boardspace");
+//             $(d).addClass("tileNatural");
+//             $(d).appendTo($(".tileArea"));
+// 		}
+//         console.log(tileList[i].value);
+//     }
 
+// }
 
+//create board objects for DOM and memory - initialization
 function createBoardspaces(){
 
     // Create Spaces that go on the board, logical plus styling aspects
@@ -122,26 +56,17 @@ function createBoardspaces(){
             d = document.createElement('div');
             var space = new Object();
 
-            var logicalX = j;
-            var realX = j*5.4*htmlFontsize;
-            var logicalY = n;
-            var realY = n*5.4*htmlFontsize;
+            space.homeX = Math.floor(j*5.4*htmlFontsize);;
+            space.homeY = Math.floor(n*5.4*htmlFontsize);
 
-            space.xVal = logicalX;
-            space.yVal = logicalY;
             space.letterVal = "";
             space.locked = 0;
 
-            $(d).css("left", realX);
-            $(d).css("top", realY);
+            $(d).css("left",  space.homeX);
+            $(d).css("top",  space.homeY);
             $(d).addClass("onboardFormat");
             $(d).addClass("boardspace");
 
-     //        $(d).mouseup(function() {
-    	// 	console.log("mouseup on " +  $(d).css("left") + " " + $(d).css("top"));
-  			// });
-
-            //deterermine if bonus and format
             var ranVal = getRandomArbitrary(0, 100);
 
             if(ranVal < 60){
@@ -184,26 +109,25 @@ function createBoardspaces(){
 
 }
 
-
-
+//create tile objects for DOM and memory - initialization
 function createTilelist(){
 
 var ranVal; 
 
-    for (var n = 0; n <= 3; n++){
+    for (var n = 0; n < 3; n++){
 
         for (var j = 0; j <= 9; j++){
 
         	var tile = new Object();
-
-        	tile.logicalX = j;
-            tile.realX = j*5.4*htmlFontsize;
-            tile.logicalY = n;
-            tile.realY = n*5.4*htmlFontsize;
+      
+            tile.homeX = Math.floor(j*5.4*htmlFontsize);
+            tile.homeY = Math.floor(n*5.4*htmlFontsize);
+            tile.realX = tile.homeX;
+            tile.realY = tile.homeY;
             tile.played = 0;
             tile.selected = 0;
 
-
+            //get ten vowels and twenty consonants 
         	if(n==0){
         		ranVal = getRandomArbitrary(0, 5);
         		tile.value = vowels.charAt(ranVal);
@@ -213,32 +137,46 @@ var ranVal;
         	}
         	tileList.push(tile);
 
+            d = document.createElement('div');
+            $(d).css("left", tile.homeX);
+            $(d).css("top", tile.homeY);
+            $(d).text(tile.value);
+            $(d).addClass("onboardFormat");
+            $(d).addClass("boardspace");
+            $(d).addClass("tileNatural");
+            $(d).appendTo($(".tileArea"));
+  
         }
             //Draw Main Play Board here
     }
-
-
-
-    // for(var i =0; i < 10; i++){
-    //     var tile = new Object();
-    //     ranVal = getRandomArbitrary(0, 5);
-    //     tile.value = vowels.charAt(ranVal);
-    //     
-    // }
-
-    // for(var k = 10; i < 30; i++){
-    //     var tile = new Object();
-    //     ranVal = getRandomArbitrary(0, 21);
-    //     tile.value = consonants.charAt(ranVal);
-    //     tileList.push(tile);
-    // }
-
-    // for(var j=0; j < 30; j++){
-    //     console.log(tileList[j].value);
-    // }
-
 }
 
+//function that gets the checker file form the server and loads it in memory inside wordArray 
+function createwordArray(){
+    var file = "./wordsEn.txt";
+    $.get(file,function(txt){ 
+        var lines = txt.split("\n");
+        for (var i = 0, len = lines.length; i < len; i++) {
+            wordArray[i]=lines[i];
+        }
+    }); 
+}
+//function that determins if a point (touch or mouseclick) is within a particular square
+function isIntersectingsquare(pntX, pntY, squareX,squareY, width) {
+	if ((pntX > squareX)&&(pntX<squareX+width)){
+		if ((pntY<squareY+width)&&(pntY>squareY)){
+            return 1;
+        }                     
+    }
+    return 0;                
+}
+
+//rand number helper function
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+//function that returns point value of a tile for scoring and formatting tiles 
 function returnPointvalue(letter){
 
     if(letter=='a'){
@@ -325,52 +263,63 @@ function returnPointvalue(letter){
     if(letter=='z'){
         return 10;
     }
-
-
 }
 
-//function that gets the checker file form the server and loads it in memory inside wordArray 
-function createwordArray(){
-    var file = "./wordsEn.txt";
-    $.get(file,function(txt){
-       
-        var lines = txt.split("\n");
+//END Methods
+//END Methods
+//END Methods
+//END Methods
 
-        for (var i = 0, len = lines.length; i < len; i++) {
-            wordArray[i]=lines[i];
+
+//Event Listners
+//Event Listners
+//Event Listners
+//Event Listners
+//When the "p" is clicked check whatever word is in the box and change the text box color - Tester
+$( ".form" ).click(function() {
+    var found = 0;
+    for(i = 0; i < wordArray.length; i++){
+        testString = wordArray[i].replace(/(\r\n|\n|\r)/gm,"");
+        //alert(testString);
+        if($(".tBox").val() == testString){
+            found = 1;
+            $(".tBox").css("background-color","green");
         }
-        // alert(wordArray.length);
+    } 
+    if(!found){
+        $(".tBox").css("background-color","red");
+    }   
+});
 
 
+$(".container").click(function(event) {//Should be mouseup when building the rest - HH
+            // console.log("mouseup on " +  $(d).css("left") + " " + $(d).css("top"));
+    
+    var relX = event.pageX - $(".container").offset().left;
+    var relY = event.pageY - $(".container").offset().top;   
+    //console.log("relX : " + relX + " Y:  " + relY);
 
-    }); 
-}
-
-function isIntersectingsquare(pntX, pntY, squareX,squareY, width) {
-
-	if ((pntX > squareX)&&(pntX<squareX+width)){
-		if ((pntY<squareY+width)&&(pntY>squareY)){
-            return 1;
-        }                     
+    for(var i=0; i < gameBoard.length; i++){
+        if(isIntersectingsquare(relX, relY, gameBoard[i].homeX, gameBoard[i].homeY, 5.0*htmlFontsize)){
+            console.log("Tile Pressed " + gameBoard[i].homeX + " " + gameBoard[i].homeY);
+        }
     }
-    return 0;                
-}
+
+});
 
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-
-// in order to work 'Math.seed' must NOT be undefined,
-// so in any case, you HAVE to provide a Math.seed
-// Math.seededRandom = function(max, min) {
-//     max = max || 1;
-//     min = min || 0;
- 
-//     Math.seed = (Math.seed * 9301 + 49297) % 233280;
-//     var rnd = Math.seed / 233280;
- 
-//     return min + rnd * (max - min);
-// }
-// http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+$(".tileNatural").click(function(event) {
+            // console.log("mouseup on " +  $(d).css("left") + " " + $(d).css("top"));
+    
+    //console.log("Tile Area Clicked"); 
+    //console.log("client X: " + event.clientX + " Y:  " + event.clientY);  
+    var relX = event.pageX - $(".tileArea").offset().left;
+    var relY = event.pageY - $(".tileArea").offset().top;   
+    //console.log("relX : " + relX + " Y:  " + relY);
+    console.log("Selected " + Math.round($(this).position().left) + " " + Math.round($(this).position().top));
+    //Determine which tile has been selected
+});
+//End Event Listners
+//End Event Listners
+//End Event Listners
+//End Event Listners
